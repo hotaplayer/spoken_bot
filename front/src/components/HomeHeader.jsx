31580e733dacb6f961dbab33718f821010b0b91e
arc0035 <aoronchu@gmail.com>
2023-04-08 23:14:06 +0800

    完成登入逻辑

--- front/src/components/HomeHeader.jsx
+++ front/src/components/HomeHeader.jsx
@@ -1,13 +1,18 @@
 import "@/styles/HomeHeader.css";
 import { Button } from "antd";
 import Logo from "@/assets/logo.png";
+import { useNavigate } from "react-router-dom";
 
 export default function HomeHeader() {
+    const navigate = useNavigate();
     return (
         <div className="home-header">
             <img src={Logo} className="w-28 h-10"/>
             <div>
-                <Button className="sbbtn bg-white border-slate-300 text-black  text-base">Log In</Button> 
+                <Button 
+                className="sbbtn bg-white border-slate-300 text-black  text-base"
+                onClick={()=>{navigate('/login')}}
+                >Log In</Button> 
             </div>
         </div>
     )
--- front/src/pages/Login.jsx
+++ front/src/pages/Login.jsx
@@ -1,4 +1,35 @@
-
+import "@/styles/Login.css";
+import { Form,Input,Button } from "antd";
+import { useForm
+ } from "antd/es/form/Form";
 export default function Login(){
-    
+
+    const form = useForm();
+
+    return (<div id="login-bg">
+        <div id="login-container"
+        >
+            <h1>欢迎登入SpokenBot</h1>
+            <Form id="login-form" >
+                <Form.Item required className="w-full">
+                    <Input type="text" placeholder="Username"  size="large"></Input>
+                </Form.Item>
+                <Form.Item required className="w-full">
+                    <Input type="password" placeholder="Password" size="large" ></Input>
+                </Form.Item>
+                <Form.Item className="self-end" >
+                    <a className="text-black underline">Forgot Password?</a>
+                </Form.Item>
+                <Form.Item className="w-full ">
+                    <Button type="primary" className="w-full bg-black text-white font-fold" >Log In</Button>
+                </Form.Item>
+                
+                <Form.Item  className="self-end ">
+                    Not register?  
+                        <a className="ml-4 underline text-black" >Register Now!</a>
+                </Form.Item>
+
+            </Form>
+        </div>
+    </div>)
 }
