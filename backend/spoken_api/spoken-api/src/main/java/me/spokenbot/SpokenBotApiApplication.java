853a8272498f7fc4c0fec1022e0bc3129fac2ed5
arc0035 <aoronchu@gmail.com>
2023-04-11 22:33:58 +0800

    init api project

---
+++ backend/spoken_api/spoken-api/src/main/java/me/spokenbot/SpokenBotApiApplication.java
@@ -0,0 +1,11 @@
+package me.spokenbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpokenBotApiApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpokenBotApiApplication.class, args);
+    }
+}
